42d1268faf7a6c4bd31c66ef76d47dd4ec19bae6
FauconSau6 <100365233+FauconSau6@users.noreply.github.com>
2022-10-28 23:19:29 -0400

    Update Dm.js

--- commands/Dm.js
+++ commands/Dm.js
@@ -1,10 +1,12 @@
 module.exports = function (message,args,bot){
-    if (args[0]== 'DM'& message.author.id == '292793422371225601'){
+    if (args[0]== 'DM'& message.author.id == ''){
+        //put your discord ID in the ''
         const user = bot.users.cache.get(args[1])
         if (user == undefined) return message.channel.send("j'ai pas trouvé l'utilisateur");
         user.send(message.content.slice(args[0].length + args[1].length + 2)) 
         message.channel.send('**Envoyer**')}
         else{
-            bot.users.cache.get("292793422371225601").send(message.author.username +": "+ message.content)
+            bot.users.cache.get("").send(message.author.username +": "+ message.content)
+            //put your discord id in the ""
         }
 }
