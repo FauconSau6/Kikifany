3e31624289d462f4706fb455843dd2f0488149ac
FauconSau6 <100365233+FauconSau6@users.noreply.github.com>
2023-02-05 10:11:50 -0500

    Update noah.js
    une nouvelle étape de la vie

--- commands/noah.js
+++ commands/noah.js
@@ -1,15 +1,2 @@
-module.exports = function(message,){
-    message.channel.send("Tout le monde t'aime sauf...");
-    setTimeout(() => {
-        message.channel.send('TI')
-    
-    }, 7000);
-    setTimeout(() => {
-        message.channel.send('FA')
-        
-    }, 7000);
-    setTimeout(() => {
-        message.channel.send('NY')
-    }, 7000);
-    
-}
+module.exports = function(message,)
+    message.channel.send("Non, il est déja trop tard pour s'enfuire");
